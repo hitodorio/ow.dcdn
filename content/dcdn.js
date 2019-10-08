cc1355e64df0124f06020f00c043609d34dbbade
hitodorio <55932487+hitodorio@users.noreply.github.com>
2019-10-09 00:30:26 +0200

    v.1
    v.1

---
+++ content/dcdn.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+// angular.js main app initialization
+var app = angular.module('myCtl', ['ngRoute']);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
+
+
+app.controller('myCtl', function($scope) {
+	  $scope.data = {"angular.js":{"1.4.2":["ang142route.js","ang142.js"]},"bootstrap":{"3.3.4":["bootstrap3.css"],"4.3.1":["bootstrap4.css","bootstrap4.js"]},"jquery":{"3.3.1":["jquery.js"]},"ng-table":{"2.0.2":["ng-table.css","ng-table.min.js"]} };
+	  });
---
+++ content/n_ow.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+// angular.js main app initialization
+var app = angular.module('myCtl', ['ngRoute']).
+    config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/', { templateUrl: 'pages/about.html', activetab: 'about' }).
+       
+        when('/about', {
+          templateUrl: 'pages/about.html',
+          
+          activetab: 'about'
+        }).
+        
+        
+        otherwise({ redirectTo: '/' });
+        
+        
+    }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+
+        $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+          $scope.part = $route.current.activetab;
+        });
+
+      
+  }]);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
+
+
+app.controller('myCtl', function($scope,$timeout) {
+	
+          
+
+});
